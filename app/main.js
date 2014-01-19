9d5ca9091c770950354752e3178cccc401b67ab8
Hector Zarco <hezarco@gmail.com>
2014-01-19 20:34:15 +0100

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,28 @@
+(function() {
+  var editor;
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init() {
+    createEditor();
+    events();
+  }
+
+  function createEditor() {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/javascript");
+  }
+
+  function events() {
+    document.getElementById("run").onclick = runCode;
+  }
+
+  function runCode() {
+    var sanitizedValue = editor.getValue().replace(/\n/g, '').replace(/'/g, '"');
+    var code = "var script = document.createElement('script');script.textContent = '" + sanitizedValue + "';document.body.appendChild(script);";
+    chrome.tabs.executeScript({
+      code: code
+    });
+  }
+})();
